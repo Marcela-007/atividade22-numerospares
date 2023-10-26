c66c38001b073c4f0aa2f8feb442f0011917df3a
Marcela-007 <marcelaoliveira446677@gmail.com>
2023-10-26 07:47:07 -0300

    atividade22-numeros pares

---
+++ marcela.py
@@ -0,0 +1,4 @@
+# Exercício Python 22: Faça um programa que mostre na tela os numeros pares entre 1 e 50 (dica estude range e seus paramentros)
+for pares in range(1,50):
+ if pares % 2 == 0:
+   print(pares)
